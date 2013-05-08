4f1b5daf0a32f044993e1c1730a90c1c91552c34
likebeta <huangdi915103@gmail.com>
2013-05-08 22:19:38 +0800

    modify config

--- php/_admin/data/config.php
+++ php/_admin/data/config.php
@@ -1,11 +1,11 @@
 <?php return array (
   'password' => '359359',
-  'host' => 'http://dropbox-likebeta.rhcloud.com/',
+  'host' => 'http://dl.dropbox.com/u/18097045/',
   'replaceDomain' => '0',
   'relativeHTML' => '0',
   'relativeCSS' => '0',
   'static' => '0',
-  'diyStatic' => 'css|js|html|png|jpg',
+  'diyStatic' => 'css|js|html|png|jpg|gif',
   'cookies' => '0',
   'agent' => '0',
   'referer' => '0',
